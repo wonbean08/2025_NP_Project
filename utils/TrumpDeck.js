4bcbc9a33152506e6ba9d68bca01c111b22a2764
minjae1025 <kjt081025@gmail.com>
2025-06-05 10:34:30 +0900

    카드 표시 및 로직 일부 구현

--- utils/TrumpDeck.js
+++ utils/TrumpDeck.js
@@ -1,20 +1,26 @@
 export let deck = [];
-
 export function makeDeck() {
     //let deck = [];
 
-    let shape=["C","D","H","S"];
-    let number=["2","3","4","5","6","7","8","9","10","A","J","K","Q"];
+    let shape = ["C", "D", "H", "S"];
+    let number = ["2", "3", "4", "5", "6", "7", "8", "9"];
 
-    for(let i of number){
-        for(let j of shape){
-            let deckID=`$i$j`;
+    for (let i of number) {
+        for (let j of shape) {
+            let deckID = `${i}${j}`;
             deck.push({
-                "id":deckID,
-                "imgPath":deckID+".png",
-                "flag":flase//뒤집을수잇는카드인지판단
+                "id": deckID,
+                "imgPath": deckID + ".png",
+                "flag": false//뒤집을 수 있는 카드인지 판단
             });
         }
     }
+}
 
+
+export function shuffle() {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
 }
--- utils/system.js
+++ utils/system.js
@@ -1,10 +1,19 @@
-import { makeDeck } from "./TrumpDeck";
+import { deck, makeDeck, shuffle } from './TrumpDeck.js';
+makeDeck();
+
+export function getDeck() {
+    //console.log(deck);
+    return deck;
+}
 
 export function match(a,b){
-    
     return false;
 }
 
 export function showFront(a){
 
+}
+
+export function shuffleTrump() {
+    shuffle();
 }
